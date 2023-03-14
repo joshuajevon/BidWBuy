c89476486f5a91856cd0ad027130c8603c9ea2a7
joshua jevon irawan <joshuajevon05@gmail.com>
2023-03-14 14:05:29 +0700

    [add]: add modal payment

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -111,7 +111,27 @@
                 <tr>
                     <td colspan="5" class="text-right">
                         <a href="{{ url('/') }}" class="btn btn-danger"> <i class="fa fa-arrow-left"></i> Continue Shopping</a>
-                        <a href="{{url('/payment')}}" class="btn btn-success">Checkout</a>
+                        <a type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Checkout</a>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmation</h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        Are you sure want to checkout?
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">No</button>
+                                        {{-- <button type="button" class="btn btn-success">Yes</button> --}}
+                                        <a type="button" href="{{url('/payment')}}" class="btn btn-success">Yes</a>
+                                </div>
+                            </div>
+                            </div>
+                            </div>
                     </td>
                 </tr>
             </tfoot>
